84f63a7ca3be031278da9cd42c351f69af48ab6f
jwmoreno <jwmoreno@indracompany.com>
2025-08-28 20:46:25 -0500

    ffff

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,28 @@
-// service-worker.js
+const CACHE_NAME = 'minimal-cache-v1';
+const urlsToCache = [
+  '/', // index o tu landing page
+  '/inquilino.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
   console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   console.log('Service Worker activado');
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  // Passthrough: no hace caching aún
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
