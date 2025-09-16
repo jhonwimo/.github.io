eb714e9a1e27d2c0291853976362a4aed92d0507
jwmoreno <jwmoreno@indracompany.com>
2025-09-16 14:07:01 -0500

    editar fecha notificacion

--- sw.js
+++ sw.js
@@ -1,23 +1,9 @@
-const CACHE_NAME = "wshome-v1";
-const urlsToCache = [
-  "/appCliente.html",
-  "/css/inquilino.css",
-  "/css/appCliente.css",
-  "/css/map.css",
-  "/js/appCliente.js",
-  "/js/map.js",
-  "./icons/logo-192.png",
-  "./icons/logo-512.png"
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  // Se instala sin guardar nada
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request))
-  );
+  // Siempre responde desde la red (sin cache)
+  event.respondWith(fetch(event.request));
 });
